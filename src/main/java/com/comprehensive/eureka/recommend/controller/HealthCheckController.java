e8f157a32ca05cb2467560d93c35e99caf60bb85
𝑠𝑒𝑜𝑏𝑜𝑖𝑛 <sbi1024@naver.com>
2025-06-01 15:27:16 +0900

    refactor : healthCheck Controller 매핑 주소 추가

--- src/main/java/com/comprehensive/eureka/recommend/controller/HealthCheckController.java
+++ src/main/java/com/comprehensive/eureka/recommend/controller/HealthCheckController.java
@@ -1,8 +1,10 @@
 package com.comprehensive.eureka.recommend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/recommend")
 @RestController
 public class HealthCheckController {
 
