edbc2773b261a97632cf71629c87f8bbe7e7b126
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-06-09 09:42:03 +0900

    feat: API 모듈 호출을 위한 RestTemplate 설정

---
+++ src/main/java/com/comprehensive/eureka/recommend/dto/base/BaseResponseDto.java
@@ -0,0 +1,21 @@
+package com.comprehensive.eureka.recommend.dto.base;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class BaseResponseDto<T> {
+
+    private Integer statusCode;
+    private String message;
+    private T data;
+
+    public static <T> BaseResponseDto<T> success(T data) {
+        return BaseResponseDto.<T>builder()
+                .statusCode(200)
+                .message("success")
+                .data(data)
+                .build();
+    }
+}
