47167a228391b71fef99e84a41162a515feddb57
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-06-10 22:52:18 +0900

    feat: 사용자 통신 성향 - 요금제 간 유사도 계산 엔진 추가

--- src/main/java/com/comprehensive/eureka/recommend/service/impl/PlanRecommendationServiceImpl.java
+++ src/main/java/com/comprehensive/eureka/recommend/service/impl/PlanRecommendationServiceImpl.java
@@ -3,13 +3,19 @@ package com.comprehensive.eureka.recommend.service.impl;
 import com.comprehensive.eureka.recommend.dto.PlanDto;
 import com.comprehensive.eureka.recommend.dto.RecommendationDto;
 import com.comprehensive.eureka.recommend.dto.UserPreferenceDto;
+import com.comprehensive.eureka.recommend.dto.response.UserDataRecordResponseDto;
 import com.comprehensive.eureka.recommend.entity.UserPreference;
 import com.comprehensive.eureka.recommend.exception.ErrorCode;
 import com.comprehensive.eureka.recommend.exception.RecommendationException;
 import com.comprehensive.eureka.recommend.repository.UserPreferenceRepository;
 import com.comprehensive.eureka.recommend.service.PlanRecommendationService;
 import com.comprehensive.eureka.recommend.service.engine.BasicRecommender;
+import com.comprehensive.eureka.recommend.service.engine.UserPlanSimilarRecommender;
+import com.comprehensive.eureka.recommend.service.util.PlanFilter;
 import com.comprehensive.eureka.recommend.util.api.PlanApiServiceClient;
+import com.comprehensive.eureka.recommend.util.api.UserApiServiceClient;
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
@@ -22,24 +28,42 @@ import org.springframework.stereotype.Service;
 public class PlanRecommendationServiceImpl implements PlanRecommendationService {
 
     private final UserPreferenceRepository userPreferenceRepository;
+
     private final PlanApiServiceClient planApiServiceClient;
+    private final UserApiServiceClient userApiServiceClient;
+
     private final BasicRecommender basicRecommender;
+    private final UserPlanSimilarRecommender userPlanSimilarRecommender;
+
+    private final PlanFilter planFilter;
 
     @Override
     public List<RecommendationDto> recommendPlan(Long userId) {
         List<PlanDto> allPlans = fetchAllPlans();
         UserPreferenceDto userPreference = fetchUserPreference(userId);
+        int userAge = getUserAge(fetchUserBirthDay(userId));
 
         if (userPreference.getPreferenceDataUsage() == null && userPreference.getPreferencePrice() == null
                 && userPreference.getPreferenceSharedDataUsage() == null) {
 
             log.info("[추천 로직 시작] userId: {} 에 대한 사용자 선호 정보가 없어, 기본 추천 로직을 실행합니다.", userId);
-            return basicRecommender.recommendPlansByAge(allPlans, userId);
+            return basicRecommender.recommendPlansByAge(allPlans, userId, userAge);
 
         } else {
             log.info("[추천 로직 시작] userId: {} 에 대한 사용자 선호 기반 추천 로직을 실행합니다.", userId);
+
+            List<PlanDto> targetPlans = planFilter.filterPlansByAge(allPlans, userAge);
+            List<UserDataRecordResponseDto> userDataHistory = fetchUserDataRecords(userId);
+
+            List<RecommendationDto> userPlanSimilarityResults = userPlanSimilarRecommender.recommendByUserPlanSimilarity(userPreference, userDataHistory, targetPlans);
+
             return null;
         }
+    }
+
+    private Integer getUserAge(LocalDate birthDay) {
+        LocalDate today = LocalDate.now();
+        return Period.between(birthDay, today).getYears();
     }
 
     private List<PlanDto> fetchAllPlans() {
@@ -61,6 +85,26 @@ public class PlanRecommendationServiceImpl implements PlanRecommendationService 
         }
 
         return convertEntityToDto(userPreferenceOptional.get());
+    }
+
+    private LocalDate fetchUserBirthDay(Long userId) {
+        try {
+            return userApiServiceClient.getUserBirthDay(userId);
+
+        } catch (Exception e) {
+            log.error("[외부 API 호출 실패] userId: {} 의 생년월일 정보 호출에 실패했습니다.", userId, e);
+            throw new RecommendationException(ErrorCode.USER_LOAD_FAILURE);
+        }
+    }
+
+    private List<UserDataRecordResponseDto> fetchUserDataRecords(Long userId) {
+        try {
+            return userApiServiceClient.getUserDataRecords(userId);
+
+        } catch (Exception e) {
+            log.error("[외부 API 호출 실패] userId: {} 의 데이터 기록 정보 호출에 실패했습니다.", userId, e);
+            throw new RecommendationException(ErrorCode.USER_DATA_RECORD_LOAD_FAILURE);
+        }
     }
 
     private UserPreferenceDto convertEntityToDto(UserPreference userPreference) {
