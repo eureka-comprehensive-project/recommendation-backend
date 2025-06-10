3489251dea40fb3f5115cb940036aa195cc44636
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-06-10 22:49:04 +0900

    feat: 요금제 혜택 목록 조회 기능 추가

--- src/main/java/com/comprehensive/eureka/recommend/util/api/PlanApiServiceClient.java
+++ src/main/java/com/comprehensive/eureka/recommend/util/api/PlanApiServiceClient.java
@@ -1,6 +1,7 @@
 package com.comprehensive.eureka.recommend.util.api;
 
 import com.comprehensive.eureka.recommend.constant.DomainConstant;
+import com.comprehensive.eureka.recommend.dto.BenefitDto;
 import com.comprehensive.eureka.recommend.dto.PlanDto;
 import com.comprehensive.eureka.recommend.dto.base.BaseResponseDto;
 import com.comprehensive.eureka.recommend.util.RestUtil;
@@ -19,6 +20,15 @@ public class PlanApiServiceClient {
         BaseResponseDto<List<PlanDto>> response = restUtil.get(
                 DomainConstant.PLAN_DOMAIN,
                 new ParameterizedTypeReference<BaseResponseDto<List<PlanDto>>>() {}
+        );
+
+        return response.getData();
+    }
+
+    public List<BenefitDto> getBenefitsByPlanId(Integer planId) {
+        BaseResponseDto<List<BenefitDto>> response = restUtil.get(
+                DomainConstant.PLAN_DOMAIN + planId + "/benefits",
+                new ParameterizedTypeReference<BaseResponseDto<List<BenefitDto>>>() {}
         );
 
         return response.getData();
