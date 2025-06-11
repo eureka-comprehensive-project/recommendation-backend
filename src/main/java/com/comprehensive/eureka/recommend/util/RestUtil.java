4750de91f3292be9bf900f987f1e3ad9fcdcc621
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-06-11 17:52:58 +0900

    feat: RestUtil WebClient 의존성 주입 및 동작 로직 수정

--- src/main/java/com/comprehensive/eureka/recommend/util/RestUtil.java
+++ src/main/java/com/comprehensive/eureka/recommend/util/RestUtil.java
@@ -4,36 +4,28 @@ import com.comprehensive.eureka.recommend.dto.base.BaseResponseDto;
 import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Component
 @RequiredArgsConstructor
 public class RestUtil {
 
-    private final RestTemplate restTemplate;
+    private final WebClient webClient;
 
     public <R> BaseResponseDto<R> get(
             String url,
             ParameterizedTypeReference<BaseResponseDto<R>> responseType
     ) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity<Void> entity = new HttpEntity<>(headers);
+        Mono<BaseResponseDto<R>> responseMono = webClient.get()
+                .uri(url)
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(responseType);
 
-        ResponseEntity<BaseResponseDto<R>> response = restTemplate.exchange(
-                url,
-                HttpMethod.GET,
-                entity,
-                responseType
-        );
-
-        return Objects.requireNonNull(response.getBody());
+        return Objects.requireNonNull(responseMono.block());
     }
 
     public <R> BaseResponseDto<R> post(
@@ -41,17 +33,14 @@ public class RestUtil {
             Object requestBody,
             ParameterizedTypeReference<BaseResponseDto<R>> responseType
     ) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity<Object> entity = new HttpEntity<>(requestBody, headers);
+        Mono<BaseResponseDto<R>> responseMono = webClient.post()
+                .uri(url)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(requestBody)
+                .retrieve()
+                .bodyToMono(responseType);
 
-        ResponseEntity<BaseResponseDto<R>> response = restTemplate.exchange(
-                url,
-                HttpMethod.POST,
-                entity,
-                responseType
-        );
-
-        return Objects.requireNonNull(response.getBody());
+        return Objects.requireNonNull(responseMono.block());
     }
 }
