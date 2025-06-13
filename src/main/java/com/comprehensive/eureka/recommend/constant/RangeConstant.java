fbd9bc15a7dfa780c22d6349b46b5d22df6f8bc4
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-06-13 13:00:39 +0900

    feat: 가중치 및 범위 상수 튜닝

--- src/main/java/com/comprehensive/eureka/recommend/constant/RangeConstant.java
+++ src/main/java/com/comprehensive/eureka/recommend/constant/RangeConstant.java
@@ -3,11 +3,11 @@ package com.comprehensive.eureka.recommend.constant;
 public class RangeConstant {
 
     public static final double MIN_PLAN_DATA = 0.3;
-    public static final double MAX_PLAN_DATA = 250.0;
+    public static final double MAX_PLAN_DATA = 150.0;
 
     public static final double MIN_PRICE = 16500.0;
-    public static final double MAX_PRICE = 150000.0;
+    public static final double MAX_PRICE = 130000.0;
 
     public static final double MIN_PLAN_SHARED_DATA = 0.0;
-    public static final double MAX_PLAN_SHARED_DATA = 180.0;
+    public static final double MAX_PLAN_SHARED_DATA = 120.0;
 }
