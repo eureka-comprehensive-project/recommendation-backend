a51304462cbbdeb83d61c5ae4d941452a5eb809c
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-06-14 14:56:23 +0900

    fix: 부가혜택 가중치 증가

--- src/main/java/com/comprehensive/eureka/recommend/constant/WeightConstant.java
+++ src/main/java/com/comprehensive/eureka/recommend/constant/WeightConstant.java
@@ -8,10 +8,10 @@ public class WeightConstant {
     public static final double NUMERIC_SIMILARITY_WEIGHT = 0.8;
     public static final double BENEFIT_SIMILARITY_WEIGHT = 0.2;
 
-    public static final double PREFERENCE_DATA_USAGE_WEIGHT = 0.35;
-    public static final double PREFERENCE_PRICE_WEIGHT = 0.35;
+    public static final double PREFERENCE_DATA_USAGE_WEIGHT = 0.30;
+    public static final double PREFERENCE_PRICE_WEIGHT = 0.25;
     public static final double PREFERENCE_SHARED_DATA_WEIGHT = 0.1;
-    public static final double PREFERENCE_BENEFITS_WEIGHT = 0.1;
+    public static final double PREFERENCE_BENEFITS_WEIGHT = 0.325;
     public static final double PREFERENCE_FAMILY_DATA_WEIGHT = 0.05;
     public static final double PREFERENCE_VALUE_ADDED_CALL_WEIGHT = 0.05;
 
