23c0b60b1df8ec1f84eeef498c0a0abd9d76bbe9
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-06-14 16:58:05 +0900

    fix: 가중치 상수 튜닝

--- src/main/java/com/comprehensive/eureka/recommend/constant/WeightConstant.java
+++ src/main/java/com/comprehensive/eureka/recommend/constant/WeightConstant.java
@@ -9,13 +9,13 @@ public class WeightConstant {
     public static final double BENEFIT_SIMILARITY_WEIGHT = 0.2;
 
     public static final double PREFERENCE_DATA_USAGE_WEIGHT = 0.30;
-    public static final double PREFERENCE_PRICE_WEIGHT = 0.25;
+    public static final double PREFERENCE_PRICE_WEIGHT = 0.30;
     public static final double PREFERENCE_SHARED_DATA_WEIGHT = 0.1;
-    public static final double PREFERENCE_BENEFITS_WEIGHT = 0.325;
+    public static final double PREFERENCE_BENEFITS_WEIGHT = 0.30;
     public static final double PREFERENCE_FAMILY_DATA_WEIGHT = 0.05;
     public static final double PREFERENCE_VALUE_ADDED_CALL_WEIGHT = 0.05;
 
-    public static final double WEIGHT_SCORE_WEIGHT = 0.4;
+    public static final double WEIGHT_SCORE_WEIGHT = 0.6;
     public static final double USER_SIMILARITY_SCORE_WEIGHT = 0.4;
-    public static final double USER_PLAN_SIMILARITY_WEIGHT = 0.6;
+    public static final double USER_PLAN_SIMILARITY_WEIGHT = 0.4;
 }
