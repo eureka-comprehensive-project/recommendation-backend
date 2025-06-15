e0b7bb845c8aa91651d75d9c0412e7794e13b12e
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-06-15 13:12:15 +0900

    feat: 무제한 상수 재정의

--- src/main/java/com/comprehensive/eureka/recommend/constant/RangeConstant.java
+++ src/main/java/com/comprehensive/eureka/recommend/constant/RangeConstant.java
@@ -1,9 +1,11 @@
 package com.comprehensive.eureka.recommend.constant;
 
 public class RangeConstant {
+    public static final int UNLIMITED = 99999;
+    public static final double MAX_DATA = 300.0;
 
     public static final double MIN_PLAN_DATA = 0.3;
-    public static final double MAX_PLAN_DATA = 150.0;
+    public static final double MAX_PLAN_DATA = 210.0;
 
     public static final double MIN_PRICE = 16500.0;
     public static final double MAX_PRICE = 130000.0;
