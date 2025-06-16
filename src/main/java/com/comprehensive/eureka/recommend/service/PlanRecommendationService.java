bf9ac20bf5c4d0266acd3933ebbd237484f684a0
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-06-16 18:13:54 +0900

    feat: 선호 요금제 기반 요금제 간 유사도 추천 로직 추가

--- src/main/java/com/comprehensive/eureka/recommend/service/PlanRecommendationService.java
+++ src/main/java/com/comprehensive/eureka/recommend/service/PlanRecommendationService.java
@@ -7,6 +7,6 @@ import java.util.List;
 
 public interface PlanRecommendationService {
 
-    RecommendationResponseDto recommendPlan(Long userId, FeedbackDto feedbackDto);
+    RecommendationResponseDto recommendPlan(Long userId, FeedbackDto feedbackDto, Integer planId);
     List<RecommendPlanDto> recommendPlanByKeyword(String keyword);
 }
