3429f8b274c9fbaeafe90c2c44a29a100fc7bec6
Jong Kyu Lee <jklee3409@khu.ac.kr>
2025-06-22 16:23:16 +0900

    fix: 피드백 시 가중치 및 성향 조정 비율 튜닝

--- src/main/java/com/comprehensive/eureka/recommend/constant/FeedbackConstant.java
+++ src/main/java/com/comprehensive/eureka/recommend/constant/FeedbackConstant.java
@@ -2,23 +2,23 @@ package com.comprehensive.eureka.recommend.constant;
 
 public class FeedbackConstant {
 
-    public static final Double DATA_PLUS_ADJUSTMENT_RATE = 1.2;
-    public static final Double DATA_MINUS_ADJUSTMENT_RATE = 0.8;
+    public static final Double DATA_PLUS_ADJUSTMENT_RATE = 1.4;
+    public static final Double DATA_MINUS_ADJUSTMENT_RATE = 0.6;
 
-    public static final Double PRICE_PLUS_ADJUSTMENT_RATE = 1.2;
-    public static final Double PRICE_MINUS_ADJUSTMENT_RATE = 0.8;
+    public static final Double PRICE_PLUS_ADJUSTMENT_RATE = 1.4;
+    public static final Double PRICE_MINUS_ADJUSTMENT_RATE = 0.6;
 
-    public static final Double DATA_PLUS_ADJUSTMENT_RATE_ANGRY = 1.5;
-    public static final Double DATA_MINUS_ADJUSTMENT_RATE_ANGRY = 0.5;
+    public static final Double DATA_PLUS_ADJUSTMENT_RATE_ANGRY = 1.7;
+    public static final Double DATA_MINUS_ADJUSTMENT_RATE_ANGRY = 0.3;
 
-    public static final Double PRICE_PLUS_ADJUSTMENT_RATE_ANGRY = 1.5;
-    public static final Double PRICE_MINUS_ADJUSTMENT_RATE_ANGRY = 0.5;
+    public static final Double PRICE_PLUS_ADJUSTMENT_RATE_ANGRY = 1.7;
+    public static final Double PRICE_MINUS_ADJUSTMENT_RATE_ANGRY = 0.3;
 
-    public static final Double BENEFIT_ADJUSTMENT_RATE = 0.25;
-    public static final Double PRICE_ADJUSTMENT_RATE = 0.25;
-    public static final Double DATA_ADJUSTMENT_RATE = 0.25;
+    public static final Double BENEFIT_ADJUSTMENT_RATE = 0.75;
+    public static final Double PRICE_ADJUSTMENT_RATE = 0.75;
+    public static final Double DATA_ADJUSTMENT_RATE = 0.75;
 
-    public static final Double BENEFIT_ADJUSTMENT_RATE_ANGRY = 1.0;
-    public static final Double PRICE_ADJUSTMENT_RATE_ANGRY = 1.0;
-    public static final Double DATA_ADJUSTMENT_RATE_ANGRY = 1.0;
+    public static final Double BENEFIT_ADJUSTMENT_RATE_ANGRY = 1.2;
+    public static final Double PRICE_ADJUSTMENT_RATE_ANGRY = 1.2;
+    public static final Double DATA_ADJUSTMENT_RATE_ANGRY = 1.2;
 }
